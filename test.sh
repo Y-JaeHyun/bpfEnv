03c8bdf6e2b51c5a0e889ec3eefdcd25359223eb
Yoo Jae Hyun <wogus3314@gmail.com>
2022-08-03 08:21:12 +0000

    add env

--- test.sh
+++ test.sh
@@ -18,15 +18,14 @@ sudo apt-get install -y vim
 
 
 mkdir ~/go
-export GOPATH=~/go
-export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin
+echo "export GOPATH=~/go" >> ~/.bashrc
+#echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> ~/.bashrc
 
 
 mkdir ~/.vim
 cd ~/.vim
 mkdir autoload ; mkdir bundle ;cd autoload
 curl -LSso pathogen.vim https://tpo.pe/pathogen.vim
-
 
 echo "execute pathogen#infect()" >> ~/.vimrc
 echo "filetype plugin indent on" >> ~/.vimrc
@@ -43,3 +42,24 @@ sudo apt-get install -y tig
 git config --global user.email "wogus3314@gmail.com"
 git config --global user.name "Yoo Jae Hyun"
 git config --global core.editor "vim"
+
+#:GoInstallBinaries
+
+cd ~/.vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+./install.sh
+
+sudo apt update
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+tar -xf Python-3.8.0.tgz
+cd Python-3.8.0
+./configure --enable-optimizations
+make -j 8
+sudo make altinstall
+
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/clang+llvm-14.0.6-aarch64-linux-gnu.tar.xz
+tar -C /usr/local -xvf clang+llvm-14.0.6-aarch64-linux-gnu.tar.xz
+
+echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin/usr/local/clang+llvm-14.0.6-linux-gnu" >> ~/.bashrc
