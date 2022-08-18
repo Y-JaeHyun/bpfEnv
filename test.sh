aa783c3b8d2156fe56ff86542afd0f7220318fd8
Yoo Jae Hyun <wogus3314@gmail.com>
2022-08-18 06:22:31 +0000

    add clang-12

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@
 sudo apt-get update -y
 sudo apt-get -y upgrade
 
-sudo apt-get install openssh-server
-sudo service ssh start
-sudo systemctl enable ssh
+#sudo apt-get install openssh-server
+#sudo service ssh start
+#sudo systemctl enable ssh
 sudo apt-get install net-tools
 
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
@@ -46,10 +46,10 @@ git config --global core.editor "vim"
 
 #:GoInstallBinaries
 
-cd ~/.vim/bundle
-git clone https://github.com/Valloric/YouCompleteMe.git
-cd YouCompleteMe
-./install.sh
+#cd ~/.vim/bundle
+#git clone https://github.com/Valloric/YouCompleteMe.git
+#cd YouCompleteMe
+#./install.sh
 
 sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
@@ -65,4 +65,12 @@ sudo make altinstall
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 sudo apt install -y libc++-15-dev libc++abi-15-dev
 
-echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin/usr/local/clang+llvm-14.0.6-linux-gnu" >> ~/.bashrc
+######################
+#printf "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-12 main" |sudo tee /etc/apt/sources.list.d/llvm-toolchain-xenial-12.list
+#wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key |sudo apt-key add -
+#sudo apt update
+#sudo apt install llvm-12 -y
+#sudo apt install clang-12 -y
+#sudo apt-get install libllvm-12-ocaml-dev libllvm12 llvm-12 llvm-12-dev llvm-12-doc llvm-12-examples llvm-12-runtime -y
+
+
